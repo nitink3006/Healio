0739bcbffe4cb8f1b8f8a5289e452a585de6cdf8
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2024-12-28 02:44:21 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["mongoose"], // Isko bundling se bahar rakhna
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ["mongoose"], // Isko bundling se bahar rakhna
+  //   },
+  // },
 });
