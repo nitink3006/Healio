265eaf85b8eebe1ca0b3c4a988feb06c14d814eb
Nitin Kumar <nitink710btechaiml2025@kccitm.edu.in>
2024-12-27 22:00:37 +0530

    last committt

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,10 +1,2 @@
 export const BASE_URL = "http://localhost:5000/api/v1";
 export const token = localStorage.getItem("token");
-
-export default {
-  build: {
-    rollupOptions: {
-      external: ["mongoose"], // Exclude mongoose from the frontend build
-    },
-  },
-};
