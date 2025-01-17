1cf5760da59b856fa7ba5cf911d64c71a6506a96
Nitin Kumar <nitink710btechaiml2025@kccitm.edu.in>
2025-01-17 21:22:10 +0530

    ........

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,2 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://healioo.onrender.com/api/v1" // Production backend
-    : "http://localhost:5000/api/v1"; // Development backend
-
+export const BASE_URL = "http://localhost:5000/api/v1";
 export const token = localStorage.getItem("token");
